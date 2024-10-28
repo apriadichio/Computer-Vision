a1057b11035330b807c2a5100b4def452196a4be
Marchio Apriadi <marchiof1202@gmail.com>
2024-10-28 13:26:16 +0700

    First Commit

---
+++ translate.py
@@ -0,0 +1,3 @@
+translate = {"cane": "dog", "cavallo": "horse", "elefante": "elephant", "farfalla": "butterfly", "gallina": "chicken", "gatto": "cat", "mucca": "cow", "pecora": "sheep", "scoiattolo": "squirrel", "dog": "cane", "cavallo": "horse", "elephant" : "elefante", "butterfly": "farfalla", "chicken": "gallina", "cat": "gatto", "cow": "mucca", "spider": "ragno", "squirrel": "scoiattolo"}
+import tensorflow as tf
+print(tf._version_)
